1790bb29e33d3441b2f4733e3c1f3ba6c7fa278b
domgwarda <domgwarda@gmail.com>
2025-05-13 20:31:53 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { setupCounter } from "./counter.ts";
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#calculator")!.innerHTML = `
+    <input type="text" id="display" class="display" disabled>
+    <div class="buttons">
+        <button class="button_number" onclick="appendNumber('1')">1</button>
+        <button class="button_number" onclick="appendNumber('2')">2</button>
+        <button class="button_number" onclick="appendNumber('3')">3</button>
+        <button class="button" onclick="appendOperation('+')">+</button>
+
+        <button class="button_number" onclick="appendNumber('4')">4</button>
+        <button class="button_number" onclick="appendNumber('5')">5</button>
+        <button class="button_number" onclick="appendNumber('6')">6</button>
+        <button class="button" onclick="appendOperation('-')">-</button>
+
+        <button class="button_number" onclick="appendNumber('7')">7</button>
+        <button class="button_number" onclick="appendNumber('8')">8</button>
+        <button class="button_number" onclick="appendNumber('9')">9</button>
+        <button class="button clear" onclick="clearDisplay()">Clear</button>
+
+        <button class="button_number" onclick="appendNumber('0')">0</button>
+        <button class="button" onclick="appendOperation('*')">*</button>
+        <button class="button" onclick="appendOperation('/')">/</button>
+        <button class="button" onclick="calculate()">=</button>
+    </div>
+`;
+
+setupCounter(
+  document.querySelector<HTMLButtonElement>("#counter")!,
+);
